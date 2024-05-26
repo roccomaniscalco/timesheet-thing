f0755ac88a46be01cd2de853c73af36bb3be6525
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-05-26 13:57:09 -0400

    remove weekStart unique constraint

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -41,7 +41,7 @@ export const timesheets = pgTable("timesheets", {
   id: serial("id").primaryKey(),
   slug: varchar("slug").$defaultFn(() => generateSlug()).notNull(),
   status: status("status").notNull(),
-  weekStart: date("week_start").unique(),
+  weekStart: date("week_start"),
   contractorId: integer("contractor_id").references(() => contractors.id),
 });
 
