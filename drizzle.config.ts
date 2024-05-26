bfd7d24d209fb17139c68fffce87ad4b36f00353
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-05-26 03:15:01 -0400

    remove log

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from "drizzle-kit";
 
-console.log("DATABASE_URL", process.env.DATABASE_URL);
-
 export default defineConfig({
   schema: "src/server/schema.ts",
   dialect: "postgresql",
