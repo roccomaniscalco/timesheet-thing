4dd0c9262d460b9de29c7f887d3c2a4264980316
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-05-29 14:09:33 -0400

    create task table

--- src/client/routes/timesheets.$id.tsx
+++ src/client/routes/timesheets.$id.tsx
@@ -11,6 +11,17 @@ import { headerBreadcrumbTunnel } from "@/client/routes/__root.js";
 import { UserButton } from "@clerk/clerk-react";
 import { useQuery } from "@tanstack/react-query";
 import { Link, createFileRoute, useParams } from "@tanstack/react-router";
+import type { InferResponseType } from "hono";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/client/components/ui/table";
 
 export const Route = createFileRoute("/timesheets/$id")({
   component: Timesheet,
@@ -48,7 +59,85 @@ function Timesheet() {
           </BreadcrumbList>
         </Breadcrumb>
       </headerBreadcrumbTunnel.In>
-      <div>Timesheet/{timesheet?.slug}</div>
+
+      {timesheet && <TaskTable tasks={timesheet.tasks}/>}
     </>
+  );
+}
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+type Tasks = InferResponseType<
+  (typeof api.contractor.timesheets)["$id"]["$get"],
+  200
+>["tasks"];
+
+type TaskTableProps = {
+  tasks: Tasks;
+};
+function TaskTable(props: TaskTableProps) {
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Day</TableHead>
+          <TableHead className="w-[50%]">Task</TableHead>
+          <TableHead className="text-right">Hours</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {props.tasks.map((task) => (
+          <TableRow key={task.id}>
+            <TableCell className="uppercase">{task.weekDay}</TableCell>
+            <TableCell className="w-[50%]">{task.name}</TableCell>
+            <TableCell className="text-right">{task.hours}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
