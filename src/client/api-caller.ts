9d13506249086dd9538d5723ccef4865137b0d7b
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2024-05-29 15:33:45 -0400

    rename api to api-routes to disambiguate

--- src/client/api-caller.ts
+++ src/client/api-caller.ts
@@ -1,4 +1,4 @@
-import type { ApiRoutesType } from "@/server/api";
+import type { ApiRoutesType } from "@/server/api-routes";
 import { hc } from "hono/client";
 
 export const { api } = hc<ApiRoutesType>("/");
